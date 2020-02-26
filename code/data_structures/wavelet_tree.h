c3317de00f34765867d5927336c40beb2102403c
Paulo Miranda <paulomirandamss12@gmail.com>
2020-02-26 13:24:04 -0300

    refactor: refactor wavelet tree

--- code/data_structures/wavelet_tree.h
+++ code/data_structures/wavelet_tree.h
@@ -8,8 +8,8 @@ private:
 	vector<int> a, b;
 public:  
   template <class MyIterator>
-	WaveletTree(MyIterator begin, MyIterator end, t_wavelet x, t_wavelet y){
-		lo = x, hi = y;
+	WaveletTree(MyIterator begin, MyIterator end, t_wavelet minX, t_wavelet maxX){
+		lo = minX, hi = maxX;
 		if(lo == hi or begin >= end) return;
 		t_wavelet mid = (lo+hi-1)/2;
 		auto f = [mid](int x){
@@ -27,36 +27,36 @@ public:
 		l = new WaveletTree(begin, pivot, lo, mid);
 		r = new WaveletTree(pivot, end, mid+1, hi);
 	} 
-	//kth smallest element in range [l, r]
+	//kth smallest element in range [i, j]
   //1-indexed
-	int kth(int l, int r, int k){
-		if(l > r) return 0;
+	int kth(int i, int j, int k){
+		if(i > j) return 0;
 		if(lo == hi) return lo;
-		int inLeft = a[r] - a[l-1];
-		int lb1 = a[l-1] + 1, rb1 = a[r];
-		int lb2 = b[l-1] + 1, rb2 = b[r];
-		if(k <= inLeft) return this->l->kth(lb1, rb1, k);
-		return this->r->kth(lb2, rb2, k-inLeft);
+		int inLeft = a[j] - a[i-1];
+		int i1 = a[i-1] + 1, j1 = a[j];
+		int i2 = b[i-1] + 1, j2 = b[j];
+		if(k <= inLeft) return l->kth(i1, j1, k);
+		return r->kth(i2, j2, k-inLeft);
 	} 
-	//Amount of numbers in the range [l, r] Less than or equal to k
+	//Amount of numbers in the range [i, j] Less than or equal to k
   //1-indexed
-	int lte(int l, int r, int k) {
-		if(l > r or k < lo) return 0;
-		if(hi <= k) return r - l + 1;
-		int lb1 = a[l-1] + 1, rb1 = a[r];
-		int lb2 = b[l-1] + 1, rb2 = b[r];
-		return this->l->lte(lb1, rb1, k) + this->r->lte(lb2, rb2, k);
+	int lte(int i, int j, int k) {
+		if(i > j or k < lo) return 0;
+		if(hi <= k) return j - i + 1;
+		int i1 = a[i-1] + 1, j1 = a[j];
+		int i2 = b[i-1] + 1, j2 = b[j];
+		return l->lte(i1, j1, k) + r->lte(i2, j2, k);
 	} 
-	//Amount of numbers in the range [l, r] equal to k
+	//Amount of numbers in the range [i, j] equal to k
   //1-indexed
-	int count(int l, int r, int k) {
-		if(l > r or k < lo or k > hi) return 0;
-		if(lo == hi) return r - l + 1;
+	int count(int i, int j, int k) {
+		if(i > j or k < lo or k > hi) return 0;
+		if(lo == hi) return j - i + 1;
     int mid = (lo+hi-1)/2;
-		int lb1 = a[l-1]+1, rb1 = a[r];
-		int lb2 = b[l-1]+1, rb2 = b[r];
-		if(k <= mid) return this->l->count(lb1, rb1, k);
-		return this->r->count(lb2, rb2, k);
+		int i1 = a[i-1]+1, j1 = a[j];
+		int i2 = b[i-1]+1, j2 = b[j];
+		if(k <= mid) return l->count(i1, j1, k);
+		return r->count(i2, j2, k);
 	}
 	~WaveletTree(){
 		delete l;
