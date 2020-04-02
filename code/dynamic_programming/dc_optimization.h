7a5d9f9537c36207dbb4ee0cadfee939bd022018
Paulo Miranda <paulomirandamss12@gmail.com>
2020-04-02 10:28:42 -0300

    feat: add D&C optimization

---
+++ code/dynamic_programming/dc_optimization.h
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int C(int i, int j);
+const int MAXN = 100010;
+const int MAXK = 110;
+const int INF = 0x3f3f3f3f;
+int dp[MAXN][MAXK];
+void calculateDP(int l, int r, int k, int opt_l, int opt_r) {
+  if(l > r) return;
+  int mid = (l+r)>>1;
+  int ans = -INF, opt;
+  for(int i=opt_l; i<=min(opt_r, mid-1); i++) {
+    if(ans < dp[i][k-1] + C(i+1, mid)) {
+      opt = i;
+      ans = dp[i][k-1] + C(i+1, mid);
+    }
+  }
+  dp[mid][k] = ans;
+  calculateDP(l, mid-1, k, opt_l, opt);
+  calculateDP(mid+1, r, k, opt, opt_r);
+}
+int solve(int n, int k){
+  for(int i=0; i<=n; i++) dp[i][0] = -INF;
+  for(int j=0; j<=k; j++) dp[0][j] = -INF;
+  dp[0][0] = 0;
+  for(int j=1; j<=k; j++)
+    calculateDP(1, n, j, 0, n-1);
+  return dp[n][k];
+}
---
+++ test/dynamic_programming/dc_optimization_test.cpp
@@ -0,0 +1,54 @@
+#include "../../code/dynamic_programming/dc_optimization.h"
+const int MAXX = 1000010;
+int mp[MAXX];
+int ans, L, R;
+int v[MAXN];
+ void add(int i){
+	if(mp[v[i]] == 0)
+		ans++;		
+	mp[v[i]]++;	
+}
+void remove(int i){
+	mp[v[i]]--;
+	if(mp[v[i]] == 0)
+		ans--;	
+}
+int C(int i, int j){
+	while(i<L) add(--L);
+	while(R<j) add(++R);
+	while(L<i) remove(L++);
+	while(j<R) remove(R--);	
+	return ans;
+}
+int dp2[MAXN][MAXK];
+int bruteforce(int n, int k){
+  if(n == 0)
+    return (k==0) ? 0 : -INF;
+  int ans = -INF;
+  for(int j=0; j<n; j++)
+    ans = max(ans, bruteforce(j, k-1) + C(j+1, n));
+  return ans;
+}
+void test1(){
+  int n = 8, k = 3;
+  int i=1;
+  for(int x: {7, 7, 8, 7, 7, 8, 1, 7})
+    v[i++] = x;
+	L=1, R=1;
+	add(1);	
+	assert(solve(n, k) == bruteforce(n, k));
+}
+void test2(){
+  int n = 7, k = 2;
+  int i=1;
+  for(int x: {1, 3, 3, 1, 4, 4, 4})
+    v[i++] = x;
+	L=1, R=1;
+	add(1);	
+	assert(solve(n, k) == bruteforce(n, k));
+}
+int main(){
+  test1();
+  test2();
+  return 0;
+}
