c7c6c12ef0f067f547f05c6924fb6adfe0f376a6
Paulo Miranda <paulomirandamss12@gmail.com>
2020-05-09 21:34:00 -0300

    feat: add module to hash

--- code/math/basic_math.h
+++ code/math/basic_math.h
@@ -22,12 +22,14 @@ void enumeratingAllSubmasks(int mask){
     cout << s << endl;
 }
 //MOD to Hash
-const uint64_t MOD = (1ll<<61) - 1;
-uint64_t modmul(uint64_t a, uint64_t b){
-	uint64_t l1 = (uint32_t)a, h1 = a>>32, l2 = (uint32_t)b, h2 = b>>32;
-	uint64_t l = l1*l2, m = l1*h2 + l2*h1, h = h1*h2;
-	uint64_t ret = (l&MOD) + (l>>61) + (h << 3) + (m >> 29) + ((m << 35) >> 3) + 1;
-	ret = (ret & MOD) + (ret>>61);
-	ret = (ret & MOD) + (ret>>61);
-	return ret-1;
-}
+namespace ModHash{
+  const uint64_t MOD = (1ll<<61) - 1;
+  uint64_t modmul(uint64_t a, uint64_t b){
+    uint64_t l1 = (uint32_t)a, h1 = a>>32, l2 = (uint32_t)b, h2 = b>>32;
+    uint64_t l = l1*l2, m = l1*h2 + l2*h1, h = h1*h2;
+    uint64_t ret = (l&MOD) + (l>>61) + (h << 3) + (m >> 29) + ((m << 35) >> 3) + 1;
+    ret = (ret & MOD) + (ret>>61);
+    ret = (ret & MOD) + (ret>>61);
+    return ret-1;
+  }
+};
