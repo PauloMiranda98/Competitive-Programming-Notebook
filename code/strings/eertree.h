aab910df87b81ffceeb468e527019a72d6a6bcaa
Paulo Miranda <paulomirandamss12@gmail.com>
2020-12-29 23:55:29 -0300

    feat: add new features in the eertree implementation

--- code/strings/eertree.h
+++ code/strings/eertree.h
@@ -4,36 +4,37 @@ const int MAXN = 100010;
 typedef long long ll;
 namespace eertree{
   struct Node {
-    int i, j, sz, suf;
-    int to[26];
+    int i, j; 
+    int sz, suf;
+    int to[26]; //Can change to vector<pii>
   };
   Node tree[MAXN];
-  int f[MAXN], cnt[MAXN];
-  int currNode, n;
-  string s;
+  int f[MAXN], cnt[MAXN], p[MAXN];
+  int currNode, n, len;
+  char s[MAXN];
   int newNode(int l, int r){
     Node &no = tree[++n];
-    f[n] = 0;
-    no.i = l, no.j = r, no.sz = r-l+1;
+    f[n] = p[n] = 0;
+    no.i = l, no.j = r;
+    no.sz = r-l+1;
     memset(no.to, 0, sizeof(no.to));
     return n;
   }
   void init(){
-    n = 0;
+    n = len = 0;
     newNode(0, -2);
     tree[1].suf = 1;
     newNode(0, -1);
     tree[2].suf = 1;
     currNode = 1;
-    s.clear();
   }
   int getId(char c){
     return c-'a';
   }
   // O(1) amortized
   void add(char c){
-    int tmp = currNode, idx = s.size(), idC = getId(c);
-    s += c;
+    int tmp = currNode, idx = len++, idC = getId(c);
+    s[idx] = c;
     while (true) { 
       int sz = tree[tmp].sz; 
       if (idx - sz >= 1 and s[idx] == s[idx-sz-1]) 
@@ -57,12 +58,17 @@ namespace eertree{
         } 
         tree[currNode].suf = tree[tmp].to[idC];
       }
+      p[currNode] = p[tree[currNode].suf] + 1;
     }
     f[currNode]++;
   }
   //Returns the total of distinct palindrome substrings
   int size(){
     return n - 2;
+  }
+  //Returns the number of the suffix that is palindrome. Online.
+  int countSuffix(){
+    return p[currNode];
   }
   // Calculates the number of equal palindromes and saves in cnt
   // Returns the total of palindrome substrings
@@ -75,15 +81,16 @@ namespace eertree{
     }
     return ans;
   }
+  // Call precompute before
   int count(int id){
     return cnt[id];
   }
   //O(N^2)
-  void show(){
+/*void show(){
     ll ans = precompute();
     cout << "Total Palindrome Substrings: " << ans << endl;
     cout << "Total of distinct palindrome substrings: " << size() << endl;
     for(int i=3; i <= n; i++)
       cout << s.substr(tree[i].i, tree[i].sz) << ": " << cnt[i] << endl;
-  }
+  } */
 };
--- test/strings/eertree_test.cpp
+++ test/strings/eertree_test.cpp
@@ -1,6 +1,9 @@
 #include "../../code/strings/eertree.h"
 void test1(){
-  assert(true); // AC: https://www.e-olymp.com/en/submissions/8182691
+  assert(true); // AC: https://www.e-olymp.com/en/submissions/8183118
+  assert(true); // AC: https://vjudge.net/problem/URAL-1960
+  assert(true); // AC: https://vjudge.net/problem/URAL-2040
+  assert(true); // AC: http://codeforces.com/contest/17/problem/E
 }
 int main() {
   test1();
