d5115a4a5112c1f18bac870204c1a465147a2511
Paulo Miranda <paulomirandamss12@gmail.com>
2021-01-09 12:10:46 -0300

    feat: add floor sum

--- code/math/basic_math.h
+++ code/math/basic_math.h
@@ -23,6 +23,27 @@ int fastPow(int base, string bigExp, int mod){
   }
   return ans;
 }
+//\sum_{i = 0}^{n - 1} floor((a * i + b)/m)
+// 0 <= n <= 10^9
+// 1 <= m <= 10^9
+// 0 <= a, b < m
+// O(log(a + b + c + d))
+ll floor_sum(ll n, ll m, ll a, ll b) {
+  ll ans = 0;
+  if (a >= m) {
+    ans += (n - 1) * n * (a / m) / 2;
+    a %= m;
+  }
+  if (b >= m) {
+    ans += n * (b / m);
+    b %= m;
+  }
+  ll y_max = (a * n + b) / m, x_max = (y_max * m - b);
+  if (y_max == 0) return ans;
+  ans += (n - (x_max + a - 1) / a) * y_max;
+  ans += floor_sum(y_max, a, m, (a - x_max % a) % a);
+  return ans;
+}
 ll gcd(ll a, ll b){ return __gcd(a, b); }
 ll lcm(ll a, ll b){ return (a / gcd(a, b)) * b; }
 void enumeratingAllSubmasks(int mask){
--- test/math/basic_math_test.cpp
+++ test/math/basic_math_test.cpp
@@ -10,7 +10,21 @@ void testFastPow(){
     }
   }
 }
+void testFloorSum(){
+  for(int a=0; a<50; a++){
+    for(int b=0; b<50; b++){
+      for(int m=max(a, b)+1; m <= 50; m++){
+        ll ans = 0;
+        for(int n = 1; n<=50; n++){
+          ans += (a*(n-1)+b)/m;
+          assert(ans == floor_sum(n, m, a, b));
+        }
+      }
+    }
+  }
+}
 int main(){
   testFastPow();
+  testFloorSum();
   return 0;
 }
