f4340e8d8b1d72660161c40f82953a374aff5ff3
Paulo Miranda <paulomirandamss12@gmail.com>
2021-02-14 20:58:59 -0300

    refactor: refactor centroid decomposition

--- code/graph/centroid_decomposition.h
+++ code/graph/centroid_decomposition.h
@@ -1,45 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 // O(N*log(N))
-struct CentroidDecomposition{
-  vector<vector<int>> adj;
-  vector<int> dad, sub;
-  vector<bool> rem;
+// Centroid Decomposition
+const int MAXN = 200010;
+namespace CD{
+  vector<int> adj[MAXN];
+  int dad[MAXN], sub[MAXN];
+  bool rem[MAXN];
   int centroidRoot, n;
-  void init(int _n){
-    n = _n;
-    adj.resize(n);
-    dad.resize(n);
-    sub.resize(n);
-    rem.assign(n, false);
-  }
-  // Return Centroid Decomposition Tree
-  vector<vector<int>> build(){
-    assert(n > 0);
-    centroidRoot = decomp(0, -1);
-    vector<vector<int>> ret(n);
-    for (int u = 0; u < n; u++){
-      if (dad[u] != u)
-        ret[dad[u]].push_back(u);
+  void init(int n1){
+    n = n1;
+    for(int i=0; i<n; i++){
+      adj[i].clear();
+      rem[i] = false;
     }
-    return ret;
-  }
-  void addEdge(int a, int b){
-    adj[a].push_back(b);
-    adj[b].push_back(a);
-  }
-  int decomp(int u, int p){
-    int sz = dfs(u, p);
-    int c = centroid(u, p, sz);
-    if (p == -1)
-      p = c;
-    dad[c] = p;
-    rem[c] = true;
-    for (auto to : adj[c]){
-      if (!rem[to])
-        decomp(to, c);
-    }
-    return c;
   }
   int dfs(int u, int p){
     sub[u] = 1;
@@ -55,7 +30,52 @@ struct CentroidDecomposition{
         return centroid(to, u, sz);
     return u;
   }
-  int operator[](int i){
-    return dad[i];
+  void getChildren(int u, int p, int d, vector<int> &v){
+    v.push_back(d);
+    for(int to: adj[u]){
+      if(rem[to] or to == p)
+        continue;
+      getChildren(to, u, d+1, v);
+    }
+  }
+  ll ans = 0;
+  int k;
+  int decomp(int u, int p){
+    int sz = dfs(u, p);
+    int c = centroid(u, p, sz);
+    if (p == -1)
+      p = c;
+    dad[c] = p;
+    rem[c] = true;
+    // Begin
+    vector<int> f(sz+1, 0);
+    f[0] = 1;
+    for (auto to : adj[c]) if (!rem[to]){
+      vector<int> v;
+      getChildren(to, c, 1, v);
+      for(int d: v){ // Query
+        if(d <= k and k-d <= sz)
+          ans += f[k-d];
+      }
+      for(int d: v) // Update
+        f[d]++;
+    }
+    // End
+    for (auto to : adj[c]){
+      if (!rem[to])
+        decomp(to, c);
+    }
+    return c;
+  }
+  void addEdge(int a, int b){
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+  // Number of k-size paths: O(N * log(N))
+  ll solve(int k1){
+    assert(n > 0);
+    ans = 0, k = k1;
+    centroidRoot = decomp(0, -1);
+    return ans;
   }
 };
--- test/graph/centroid_decomposition_test.cpp
+++ test/graph/centroid_decomposition_test.cpp
@@ -1,31 +1,14 @@
 #include "../../code/graph/centroid_decomposition.h"
-vector<vector<int>> adj;
-bool ok;
-int verify(int u){
-  int sz = 1;
-  int mx = 0;
-  for(int to: adj[u]){
-    int aux = verify(to);
-    mx = max(mx, aux);
-    sz += aux;
-  }
-  ok = ok and (mx<=sz/2);
-  return sz;
-}
 void test1(){
-  CentroidDecomposition cd;
-  int n = 1000;
-  cd.init(n);
-  srand(time(0));
-  for (int i = 1; i < n; i++)
-    cd.addEdge(i, rand()%i);
-  adj = cd.build();  
-  ok = true;
-  verify(cd.centroidRoot);
-  assert(ok);
+  int n = 5, k = 2;
+  CD::init(n);
+  CD::addEdge(0, 1);
+  CD::addEdge(1, 2);
+  CD::addEdge(2, 3);
+  CD::addEdge(2, 4);
+  assert(CD::solve(k) == 4);
 }
 int main(){
-  for(int i=0; i<10; i++)
-    test1();
+  test1();
   return 0;
 }
